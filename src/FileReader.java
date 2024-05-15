3054a4859284f771a1d77ac5daef9203bae34ce2
Yū <artembezzzz@gmail.com>
2024-05-15 22:40:32 +0200

    a

---
+++ src/FileReader.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileReader {
+    public static ArrayList<String> getFiles(String path) {
+        ArrayList<String> fileNames = new ArrayList<String>();
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        for (File f : files) {
+            if (f.isFile()) {
+                if (f.getName().endsWith(".jpg") ||
+                        f.getName().endsWith(".png") ||
+                        f.getName().endsWith(".jpeg")) {
+                    fileNames.add(f.getName());
+                }
+            }
+        }
+        return fileNames;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> picNames = FileReader.getFiles("D:\\Users\\User\\Pictures");
+    }
+}
